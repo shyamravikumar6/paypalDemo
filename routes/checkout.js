3a73e6b724f11cacff030dbbe9ccd0caf5de264e
shyamravikumar6 <shyamravikumar6@gmail.com>
2021-01-28 21:35:02 +0530

    new commit

---
+++ routes/checkout.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const braintree = require('braintree');
+
+router.post('/', (req, res, next) => {
+  const gateway = new braintree.BraintreeGateway({
+    environment: braintree.Environment.Sandbox,
+    // Use your own credentials from the sandbox Control Panel here
+    merchantId: '2kv4gnc38gr2mc6f',
+    publicKey: 'hk4mr42wyh8vbhkf',
+    privateKey: '200d19d3d59c65770ce4ad9de36d8867'
+  });
+
+  // Use the payment method nonce here
+  const nonceFromTheClient = req.body.paymentMethodNonce;
+  // Create a new transaction for $10
+  const newTransaction = gateway.transaction.sale({
+    amount: '10.00',
+    paymentMethodNonce: nonceFromTheClient,
+    options: {
+      // This option requests the funds from the transaction
+      // once it has been authorized successfully
+      submitForSettlement: true
+    }
+  }, (error, result) => {
+      if (result) {
+        res.send(result);
+      } else {
+        res.status(500).send(error);
+      }
+  });
+});
+
+module.exports = router;
